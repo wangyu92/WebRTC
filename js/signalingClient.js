6036d089374347eba5370e9f3e91e838623c6ecd
Wangyu Choi <choiwk1013@gmail.com>
2019-04-16 15:49:16 +0900

    added comments

--- js/signalingClient.js
+++ js/signalingClient.js
@@ -1,19 +1,43 @@
 'use strict';
 
+/**
+ * 시그널링에 관련한 프로세스는 다 이 클래스에서 처리.
+ * 예를 들어서 main.js에서 해당 클래스의 인스턴스를 생성하여 시그널링 프로세스를 처리.
+ */
 class SignalingClient {
+
+    /**
+     * [constructor description]
+     * ICE Server를 지정하여 클래스의 인스턴스를 생성.
+     * @param {[type]} iceServers [description]
+     */
     constructor(iceServers) {
         this._server = "https://ms.hanyang.ac.kr:8800";
         this._iceServer = iceServers;
         this._socket = io(this._server);
+        this._initiator = false;
+        this._channelReady = false;
 
         this.addEventListenerOfSocekt();
     }
 
-    createRoom(name) {
+    /**
+     * [joinRoom description]
+     * 방에 참여하기위한 함수. 만약 생성된 방이 서버에 없을 경우, 방이 새로 생성된다.
+     * @param  {[type]} name 방 이름
+     * @return {[type]}      [description]
+     */
+    joinRoom(name) {
         this._roomName = name;
         this._socket.emit('create or join', this._roomName);
     }
 
+    /**
+     * [checkICEServer description]
+     * ICE 서버가 정상적으로 동작하는지 체크하는 함수.
+     * @param  {[type]} timeout [description]
+     * @return {[type]}         [description]
+     */
     checkICEServer(timeout) {
         return new Promise(function(resolve, reject) {
 
@@ -42,23 +66,27 @@ class SignalingClient {
         });
     }
 
+    /**
+     * [addEventListenerOfSocekt description]
+     * 리스너를 붙여주는 함수. 객체 생성 후 서버와 연결이 되고나면 반드시 실행해준다.
+     */
     addEventListenerOfSocekt() {
         //  방 생성자가 방 생성에 성공하면 수신
         this._socket.on('created', function(room) {
-            isInitiator = true;
+            this._initiator = true;
         });
 
         //  방 생성자가 아닌 사람이 join하면 수신
         this._socket.on('joined', function(room) {
             console.log('joined: ' + room);
-            isChannelReady = true;
+            this._channelReady = true;
         });
 
         //  다른 피어가 접속하면 수신
         this._socket.on('join', function(room) {
             console.log('Another peer made a request to join room ' + room);
             console.log('This peer is the initiator of room ' + room + '!');
-            isChannelReady = true;
+            this._channelReady = true;
         });
 
 
